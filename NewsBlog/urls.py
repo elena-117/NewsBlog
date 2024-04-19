59077b06cdf395b3fb59f2c67838da61d3dbbf6d
Olena Shatska <lena.shatskaya@gmail.com>
2024-04-19 04:26:45 -0600

    Updating settings.py and url.py for media files

--- NewsBlog/urls.py
+++ NewsBlog/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('', include('main.urls')),
     path('news/', include('news.urls')),
     path('', include('authsystem.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
