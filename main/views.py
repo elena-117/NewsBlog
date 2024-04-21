41e1344a48904d5df23053cef07bbb2d72170658
Olena Shatska <lena.shatskaya@gmail.com>
2024-04-21 15:54:17 -0600

    Added views for display of news grid on home page and Search box

--- main/views.py
+++ main/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
+from news.models import Articles
 
 # Create your views here.
-data = {
-    'title': 'Main page!',
-}
 
 def index(request):
+    data = {
+       'title': 'Main page!',
+       'news': Articles.objects.order_by('-date')[:9]
+    }
     return render(request, 'main/index.html', data)
+
+
+def search_articles(request):
+    if request.method == 'POST':
+        searched = request.POST['searched']
+        articles = Articles.objects.filter(title__contains=searched)
+        return render(request, 'main/search-articles.html', {'searched':searched, 'articles':articles})
+    else:
+        return render(request, 'main/search-articles.html', {})
 
 
 def about(request):
