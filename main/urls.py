8dc9716a49e29468ef25cba5b0513c75e5b615fa
Olena Shatska <lena.shatskaya@gmail.com>
2024-04-21 15:58:35 -0600

    Added path for Search

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('about', views.about, name='about'),
+    path('search-articles', views.search_articles, name='search-articles'),
 ]
